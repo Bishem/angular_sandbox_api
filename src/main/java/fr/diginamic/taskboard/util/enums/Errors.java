34cb7ba904fb609d9a7cd841bdcfb18f0d406e18
Bishem <aminrsaidali@gmail.com>
2022-08-11 02:58:38 +0200

    added to string method

--- src/main/java/fr/diginamic/taskboard/util/enums/Errors.java
+++ src/main/java/fr/diginamic/taskboard/util/enums/Errors.java
@@ -1,11 +1,14 @@
 package fr.diginamic.taskboard.util.enums;
 
+import lombok.ToString;
+
+@ToString
 public enum Errors {
 
 	ID_IS_NULL("must provide an id to perform said operation"),
 	ID_NOT_NULL("must not provide an id to perform said operation");
 
-	public final String message;
+	private final String message;
 
 	Errors(final String message) {
 		this.message = message;
